f384b20ea6b97ff63073c2e5cca93c28fef72585
HarveyPuyong <harveyluslos80@gmail.com>
2024-03-10 22:20:13 +0800

    My 3rd song

---
+++ song3.php
@@ -0,0 +1,91 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>Song3</title>
+</head>
+  <body>
+    <div class="song3-container">
+      <div class="song3-title-container"><h1>"The Saltwater Room"</h1></div>
+      <div  class="song3-detail-container"><p>("Maybe I'm Dreaming" Version)</p><br><br></div>
+
+      <div class="lyrics3">
+        <?php
+          echo "I opened my eyes last night<br/>";
+          echo "And saw you in the low light<br/>";
+          echo "Walking down by the bay, on the shore,<br/>";
+          echo "Staring up at the planes that aren't there anymore<br/><br/>";
+
+          echo "I was feeling the night grow old<br/>";
+          echo "And you were looking so cold<br/>";
+          echo "Like an introvert<br/>";
+          echo "I drew my over shirt<br/>";
+          echo "Around my arms and began to shiver violently before...<br/><br/>";
+
+          echo "... you happened to look and see<br/>";
+          echo "The tunnels all around me<br/>";
+          echo "Running into the dark underground<br/>";
+          echo "All the subways around create a great sound<br/><br/>";
+
+          echo "To my motion fatigue farewell,<br/>";
+          echo "With your ear to a seashell<br/>";
+          echo "You can hear the waves<br/>";
+          echo "In underwater caves<br/>";
+          echo "As if you actually were inside a saltwater room<br/><br/>";
+
+          echo "Time together is just never quite enough<br/>";
+          echo "When you and I are alone,<br/>";
+          echo "I've never felt so at home<br/>";
+          echo "What will it take to make or break this hint of love?<br/>";
+          echo "We need time, only time<br/><br/>";
+
+          echo "When we're apart whatever are you thinking of?<br/>";
+          echo "If this is what I call home,<br/>";
+          echo "Why does it feel so alone?<br/>";
+          echo "So tell me, darling, do you wish we'd fall in love?<br/>";
+          echo "All the time, all the time<br/><br/>";
+
+          echo "Can you believe that the crew has gone<br/>";
+          echo "And wouldn't let me sign on?<br/>";
+          echo "All my islands have sunk in the deep,<br/>";
+          echo "So I can hardly relax or even oversleep<br/><br/>";
+
+          echo "I feel as if I were home some nights,<br/>";
+          echo "When we count all the ship lights.<br/>";
+          echo "I guess I'll never know<br/>";
+          echo "Why sparrows love the snow<br/>";
+          echo "We'll turn off all of the lights<br/>";
+          echo "And set this ballroom aglow<br/><br/>";
+
+          echo "(So tell me darling, do you wish we'd fall in love?All the time)<br/><br/>";
+
+          echo "Time together is just never quite enough<br/>";
+          echo "When you and I are alone,<br/>";
+          echo "I've never felt so at home<br/>";
+          echo "What will it take to make or break this hint of love?<br/>";
+          echo "We need time, only time<br/><br/>";
+
+          echo "When we're apart whatever are you thinking of?<br/>";
+          echo "If this is what I call home,<br/>";
+          echo "Why does it feel so alone?<br/>";
+          echo "So tell me, darling, do you wish we'd fall in love?<br/>";
+          echo "All the time, all the time<br/><br/>";
+
+          echo "Time together is just never quite enough<br/>";
+          echo "When we're apart whatever are you thinking of?<br/>";
+          echo "What will it take to make or break this hint of love?<br/>";
+          echo "So tell me, darling, do you wish we'd fall in love?<br/>";
+          echo "All the time<br/>";
+        ?>
+    </div>
+  </div>
+    
+  </body>
+</html>
+
+
+
+
+
+
