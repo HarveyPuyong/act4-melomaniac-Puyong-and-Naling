b6bc7e53e651b20a364df08a27b3e6e9b6749cd0
HarveyPuyong <harveyluslos80@gmail.com>
2024-03-10 22:21:59 +0800

    My 4th song

---
+++ song4.php
@@ -0,0 +1,89 @@
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Song4</title>
+  </head>
+  <body>
+    <div class="song4-container">
+      <div class="song4-title-container"><h1>"Cruel Summer"</h1></div>
+      <div class="lyrics4">
+        <?php
+          echo "(Yeah, yeah, yeah, yeah)<br/><br/>";
+
+          echo "Fever dream high in the quiet of the night<br/>";
+          echo "You know that I caught it (Oh yeah, you're right, I want it)<br/>";
+          echo "Bad, bad boy, shiny toy with a price<br/>";
+          echo "You know that I bought it (Oh yeah, you're right, I want it)<br/><br/>";
+
+          echo "Killing me slow, out the window<br/>";
+          echo "I'm always waiting for you to be waiting below<br/>";
+          echo "Devils roll the dice, angels roll their eyes<br/>";
+          echo "What doesn't kill me makes me want you more<br/><br/>";
+
+          echo "And it's new, the shape of your body<br/>";
+          echo "It's blue, the feeling I've got<br/>";
+          echo "And it's ooh, whoa oh<br/>";
+          echo "It's a cruel summer<br/>";
+          echo "It's cool, that's what I tell 'em<br/>";
+          echo "No rules in breakable heaven<br/>";
+          echo "But ooh, whoa oh<br/>";
+          echo "It's a cruel summer<br/>";
+          echo "With you<br/><br/>";
+
+          echo "Hang your head low in the glow of the vending machine<br/>";
+          echo "I'm not dying (Oh yeah, you're right, I want it)<br/>";
+          echo "We say that we'll just screw it up in these trying times<br/>";
+          echo "We're not trying (Oh yeah, you're right, I want it)<br/><br/>";
+
+          echo "So cut the headlights, summer's a knife<br/>";
+          echo "I'm always waiting for you just to cut to the bone<br/>";
+          echo "Devils roll the dice, angels roll their eyes<br/>";
+          echo "And if I bleed, you'll be the last to know<br/><br/>";
+
+          echo "Oh, it's new, the shape of your body<br/>";
+          echo "It's blue, the feeling I've got<br/>";
+          echo "And it's ooh, whoa oh<br/>";
+          echo "It's a cruel summer";
+          echo "It's cool, that's what I tell 'em<br/>";
+          echo "No rules in breakable heaven<br/>";
+          echo "But ooh, whoa oh<br/>";
+          echo "It's a cruel summer<br/>";
+          echo "With you<br/><br/>";
+
+          echo "I'm drunk in the back of the car<br/>";
+          echo "And I cried like a baby coming home from the bar (Oh)<br/>";
+          echo "Said, I'm fine, but it wasn't true<br/>";
+          echo "I don't wanna keep secrets just to keep you";
+          echo "And I snuck in through the garden gate<br/>";
+          echo "Every night that summer just to seal my fate (Oh)<br/>";
+          echo "And I scream, For whatever it's worth<br/>";
+          echo "I love you, ain't that the worst thing you ever heard?<br/>";
+          echo "He looks up, grinning like a devil<br/><br/>";
+
+          echo "it's new, the shape of your body<br/>";
+          echo "It's blue, the feeling I've got<br/>";
+          echo "And it's ooh, whoa oh<br/>";
+          echo "It's a cruel summer";
+          echo "It's cool, that's what I tell 'em<br/>";
+          echo "No rules in breakable heaven<br/>";
+          echo "But ooh, whoa oh<br/>";
+          echo "It's a cruel summer<br/>";
+          echo "With you<br/><br/>";
+          
+          echo "I'm drunk in the back of the car<br/>";
+          echo "And I cried like a baby coming home from the bar (Oh)<br/>";
+          echo "Said, I'm fine, but it wasn't true<br/>";
+          echo "I don't wanna keep secrets just to keep you";
+          echo "And I snuck in through the garden gate<br/>";
+          echo "Every night that summer just to seal my fate (Oh)<br/>";
+          echo "And I scream, For whatever it's worth<br/>";
+          echo "I love you, ain't that the worst thing you ever heard?<br/>";
+          echo "(Yeah, yeah, yeah, yeah)<br/>";
+        ?> 
+      </div>
+    </div>  
+  </body>
+</html>
+
