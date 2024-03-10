08d791e04ae7e768f9b00271875c5e8962b2894b
HarveyPuyong <harveyluslos80@gmail.com>
2024-03-10 22:39:58 +0800

    This page contains links to all song

---
+++ index.php
@@ -0,0 +1,28 @@
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Activity5</title>
+  </head>
+  <body>
+    <h1 class="melomaniac">Melomaniac</h1>
+      <div class="song-list-container">
+        <div class="songs">
+          <a href="song1.php" title="SongName:'Everything Goes On'">Song1</a>
+        </div>
+        <div class="songs">
+          <a href="song2.php" title="SongName:'Sunflower'">Song2</a>
+        </div>
+        <div class="songs">
+          <a href="song3.php" title="SongName:'The Saltwater Room'">Song3</a>
+        </div>
+        <div class="songs">
+          <a href="song4.php" title="SongName:'Cruel Summer'">Song4</a>
+        </div>
+        <div class="songs">
+          <a href="song5.php" title="SongName:'Something Just Like This'">Song5</a>
+        </div> 
+      </div>
+  </body>
+</html>
