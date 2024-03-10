c1f7e2254aeabce2ee4999921a25cae4b7639e83
HarveyPuyong <harveyluslos80@gmail.com>
2024-03-10 22:22:43 +0800

    My 5th song

---
+++ song5.php
@@ -0,0 +1,92 @@
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Song5</title>
+  </head>
+  <body>
+    <div class="song5-container">
+      <div class="song5-title-container"><h1>"Something Just Like This"</h1></div>
+      <div  class="song5-detail-container"> <p>(with Coldplay)</p><br></div>
+
+      <div class="lyrics5">
+    <?php
+      echo "(Yeah, yeah, yeah, yeah)<br/><br/>";
+
+      echo "I've been reading books of old<br/>";
+      echo "The legends and the myths<br/>";
+      echo "Achilles and his gold<br/>";
+      echo "Hercules and his gifts<br/>";
+      echo "Spiderman's control<br/>";
+      echo "And Batman with his fists<br/>";
+      echo "And clearly I don't see myself upon that list<br/><br/>";
+
+      echo "But she said, Where'd you wanna go?<br/>";
+      echo "How much you wanna risk?<br/>";
+      echo "I'm not looking for somebody<br/>";
+      echo "With some superhuman gifts<br/>";
+      echo "Some superhero<br/>";
+      echo "Some fairytale bliss<br/>";
+      echo "Just something I can turn to<br/>";
+      echo "Somebody I can kiss<br/>";
+      echo "I want something just like this<br/><br/>";
+
+      echo "Doo-doo-doo, doo-doo-doo<br/>";
+      echo "Doo-doo-doo, doo-doo<br/>";
+      echo "Doo-doo-doo, doo-doo-doo<br/>";
+      echo "Oh, I want something just like this<br/>";
+      echo "Doo-doo-doo, doo-doo-doo<br/>";
+      echo "Doo-doo-doo, doo-doo<br/>";
+      echo "Doo-doo-doo, doo-doo-doo<br/><br/>";
+
+      echo "Oh, I want something just like this<br/>";
+      echo "I want something just like this<br/><br/>";
+
+      echo "I've been reading books of old<br/>";
+      echo "The legends and the myths<br/>";
+      echo "The testaments they told<br/>";
+      echo "The moon and its eclipse<br/>";
+      echo "And Superman unrolls<br/>";
+      echo "A suit before he lifts<br/><br/>";
+      echo "But I'm not the kind of person that it fits<br/><br/>";
+
+      echo "She said, Where'd you wanna go?<br/>";
+      echo "How much you wanna risk?<br/>";
+      echo "I'm not looking for somebody<br/>";
+      echo "With some superhuman gifts";
+      echo "Some superhero<br/>";
+      echo "Some fairytale bliss<br/>";
+      echo "Just something I can turn to<br/>";
+      echo "Somebody I can miss<br/>";
+      echo "I want something just like this<br/>";
+      echo "I want something just like this<br/><br/>";
+
+      echo "Oh, I want something just like this<br/>";
+      echo "Doo-doo-doo, doo-doo-doo<br/>";
+      echo "Doo-doo-doo, doo-doo<br/>";
+      echo "Doo-doo-doo, doo-doo-doo<br/>";
+      echo "Oh, I want something just like this<br/>";
+      echo "Doo-doo-doo, doo-doo-doo<br/>";
+      echo "Doo-doo-doo, doo-doo<br/>";
+      echo "Doo-doo-doo, doo-doo-doo<br/><br/>";
+
+      echo "Where'd you wanna go?<br/>";
+      echo "How much you wanna risk?<br/>";
+      echo "I'm not looking for somebody<br/>";
+      echo "With some superhuman gifts<br/>";
+      echo "Some superhero<br/>";
+      echo "Some fairytale bliss<br/>";
+      echo "Just something I can turn to<br/>";
+      echo "Somebody I can kiss<br/>";
+      echo "I want something just like this<br/><br/>";
+
+      echo "Oh, I want something just like this<br/>";
+      echo "Oh, I want something just like this<br/>";
+      echo "Oh, I want something just like this<br/>";
+      ?> 
+    </div>
+   </div>
+  </body>
+</html>
+
