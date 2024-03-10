bec23b398087f1b5cd8d599e4d163184a3552c3d
HarveyPuyong <harveyluslos80@gmail.com>
2024-03-10 22:19:02 +0800

    My 2nd song

---
+++ song2.php
@@ -0,0 +1,78 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>Song2</title>
+</head>
+  <body>
+    <div class="song2-container">
+   <div class="song2-title-container"><h1 class="song2-title">"Sunflower"</h1></div>
+   <div  class="song2-detail-container"><p> (from "Spider-Man: Into The Spider-Verse" soundtrack)</p><br><br></div>
+   <div class="lyrics2">
+    <?php
+      echo "Ayy, ayy, ayy, ayy (ooh)<br/>";
+      echo "Ooh, ooh, ooh, ohh (ooh)<br/>";
+      echo "Ayy, ayy<br/>";
+      echo "Ooh, ooh, ooh, ohh<br/><br>";
+
+      echo "Needless to say, I keep in check<br/>";
+      echo "She was a bad-bad, nevertheless (yeah)<br/>";
+      echo "Callin' it quits now, baby, I'm a wreck (wreck)<br/>";
+      echo "Crash at my place, baby, you're a wreck (wreck)<br/><br>";
+
+      echo "Needless to say, I'm keepin' in check<br/>";
+      echo "She was a bad-bad, nevertheless<br/>";
+      echo "Callin' it quits now, baby, I'm a wreck<br/>";
+      echo "Crash at my place, baby, you're a wreck<br/><br>";
+
+      echo "Thinkin' in a bad way, losin' your grip<br/>";
+      echo "Screamin' at my face, baby, don't trip<br/>";
+      echo "Someone took a big L, don't know how that felt<br/>";
+      echo "Lookin' at you sideways, party on tilt<br/><br>";
+
+      echo "Ooh-ooh<br/>";
+      echo "Some things you just can't refuse<br/>";
+      echo "She wanna ride me like a cruise<br/>";
+      echo "And I'm not tryna lose<br/><br>";
+
+      echo "Then you're left in the dust<br/>";
+      echo "Unless I stuck by ya<br/>";
+      echo "You're a sunflower<br/>";
+      echo "I think your love would be too much<br/>";
+      echo "Or you'll be left in the dust<br/>";
+      echo "Unless I stuck by ya<br/>";
+      echo "You're the sunflower<br/>";
+      echo "You're the sunflower<br/><br>";
+
+      echo "Every time I'm leavin' on you<br/>";
+      echo "You don't make it easy, no, no<br/>";
+      echo "Wish I could be there for you<br/>";
+      echo "Give me a reason to, oh, oh<br/><br>";
+
+      echo "Every time I'm walkin' out<br/>";
+      echo "I can hear you tellin' me to turn around<br/>";
+      echo "Fightin' for my trust and you won't back down<br/>";
+      echo "Even if we gotta risk it all right now, oh<br/><br>";
+
+      echo "I know you're scared of the unknown (-known)<br/>";
+      echo "You don't wanna be alone (alone)<br/>";
+      echo "I know I always come and go (and go)<br/>";
+      echo "But it's out of my control<br/><br>";
+
+      echo "And you'll be left in the dust<br/>";
+      echo "Unless I stuck by ya<br/>";
+      echo "You're a sunflower<br/>";
+      echo "I think your love would be too much<br/>";
+      echo "Or you'll be left in the dust<br/>";
+      echo "Unless I stuck by ya<br/>";
+      echo "You're the sunflower<br/>";
+      echo "You're the sunflower<br/>";
+      echo "Yeah<br/>";
+    ?>
+  </div>
+  </div>
+    
+  </body>
+</html>
+
