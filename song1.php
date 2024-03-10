d2d89dafb2ab88c67b44118575b036dafa77de53
HarveyPuyong <harveyluslos80@gmail.com>
2024-03-10 22:18:18 +0800

    My 1st song

---
+++ song1.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>Song1</title>
+</head>
+  <body>
+    <div class="song1-container">
+      <div class="song1-title-container"><h1>"Everything Goes On"</h1></div>
+      <div  class="song1-detail-container"><p> (from "League Of Legends: Star Guardian" soundtrack)</p><br><br>
+      </div>
+      <div class="lyrics1">
+        <?php
+          echo "Don't try to make yourself remember, darling<br/>";
+          echo "Don't look for me, I'm just a story you've been told<br/>";
+          echo "So let's pretend a little longer<br/>";
+          echo "Cause when we're gone, everything goes on<br/><br>";
+
+          echo "Well, you were staring at the window<br/>";
+          echo "And I said: \"I wanna take you to the sea\"<br/>";
+          echo "And when I'm better, we'll do everything<br/>";
+          echo "I gotta stop making promises I can't keep<br/><br>";
+
+          echo "But if I was gone tomorrow, won't the waves crash on?<br/>";
+          echo "Is it selfish that I'm happy as we pass the setting sun?<br/>";
+          echo "Someday I'll be overcast, but you won't have to cry<br/>";
+          echo "Cause we'll do the grieving, but I'm by your side, I said:<br/><br>";
+
+          echo "Don't try to make yourself remember, darling<br/>";
+          echo "Don't look for me, I'm just a story you've been told<br/>";
+          echo "So let's pretend a little longer<br/>";
+          echo "Cause when you're gone, everything goes on<br/><br>";
+
+          echo "And I'm so scared of getting used to this<br/>";
+          echo "All the vines that keep you tethered in your room<br/>";
+          echo "Someday both of us are leaving here<br/>";
+          echo "But for now I'll just bring the sea to you<br/><br>";
+
+          echo "When you meet me in the daylight, like we did before<br/>";
+          echo "Then I felt you on my shoulder, and you weren't suffering anymore<br/>";
+          echo "You said: \"I'm sorry that you worry, but don't apologize\"<br/>";
+          echo "I told you to forget me, but you stayed by my side, when I said:<br/><br>";
+
+          echo "Don't try to make yourself remember, darling<br/>";
+          echo "Don't look for me, I'm just a story you've been told<br/>";
+          echo "So let's pretend a little longer<br/>";
+          echo "Cause when we're gone, everything goes on<br/>";
+        ?>
+      </div>
+   
+    
+  </div>
+  </body>
+</html>
